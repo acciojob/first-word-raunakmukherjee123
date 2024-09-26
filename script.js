b9fdfe590def9c9f90c7ee7b81255e6971a5f689
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-26 21:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function firstWord(s) {
-  // your code here
+	
+	if(s.length==0)
+	{
+		return s;
+	}
+	else if(s.includes(" ",0)==false)
+	{
+		return s;
+	}
+	else
+	{
+      let str;
+	  s=s.trim();
+      for(int i=0;i<s.length;i++)
+		{
+			let ch=s.charAt(i);
+			
+			if(ch==' ')
+			{
+				return str;
+			}
+			str+=ch;
+		}
+    }
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
